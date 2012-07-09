fe411fa969571d963b59b2c9790ad0539d64ab11
Bill DeRusha <bill.derusha@gmail.com>
2012-07-09 10:06:31 -0400

    twitter response to_s

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -28,12 +28,12 @@ class Tweet < ActiveRecord::Base
 		puts "#{u.id}: #{u.t_screen_name}"
 		tw = Tweet.find_or_create_by_t_id(t.id)
 		unless tw.message == t.text and 
-			tw.in_reply_to_user_id == t.in_reply_to_user_id_str and
-			tw.in_reply_to_status_id == t.in_reply_to_status_id_str and
+			tw.in_reply_to_user_id == t.in_reply_to_user_id.to_s and
+			tw.in_reply_to_status_id == t.in_reply_to_status_id.to_s and
 			tw.user_id == u.id
 				tw.update_attributes(:message => t.text,
-					:in_reply_to_user_id => t.in_reply_to_user_id_str,
-					:in_reply_to_status_id => t.in_reply_to_status_id_str,
+					:in_reply_to_user_id => t.in_reply_to_user_id.to_s,
+					:in_reply_to_status_id => t.in_reply_to_status_id.to_s,
 					:user_id => u.id)
 		end
 
